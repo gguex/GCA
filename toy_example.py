927beb022ce9c8ba8e5e1cb3344b73393cdcad52
gguex <guillaume.guex@gmail.com>
2025-03-26 11:09:19 +0100

    write the toy example

---
+++ toy_example.py
@@ -0,0 +1,55 @@
+# ---------------------------------------------------
+# Toy example for the generalized CA
+# ---------------------------------------------------
+
+import numpy as np
+from src.ca import correspondence_analysis
+import matplotlib.pyplot as plt
+
+# Create a contingency table
+n_r = 7
+n_c = 4
+cont_table = np.random.randint(0, 11, (n_r, n_c))
+
+# Create the grouping matrix
+Z_crisp = np.array([[1, 0, 0],
+                    [1, 0, 0],
+                    [1, 0, 0],
+                    [0, 1, 0],
+                    [0, 1, 0],
+                    [0, 0, 1],
+                    [0, 0, 1]])
+n_g = 3
+
+# Create the crisp similarity 
+S_crisp = Z_crisp @ Z_crisp.T
+
+# Perform the CA
+res = correspondence_analysis(cont_table)
+
+# Perform the grouped CA
+cont_table_gpd = Z_crisp.T @ cont_table
+res_gpd = correspondence_analysis(cont_table_gpd)
+
+# Perform the 
+
+# Plot the CA results
+fig, ax = plt.subplots()
+ax.scatter(res['row_coord'][:, 0], res['row_coord'][:, 1], color='blue')
+ax.scatter(res['col_coord'][:, 0], res['col_coord'][:, 1], color='red')
+for i in range(n_r):
+    ax.text(res['row_coord'][i, 0], res['row_coord'][i, 1], f'Row {i+1}')
+for j in range(n_c):
+    ax.text(res['col_coord'][j, 0], res['col_coord'][j, 1], f'Col {j+1}')
+plt.show()
+
+# Plot the grouped CA results
+fig, ax = plt.subplots()
+ax.scatter(res_gpd['row_coord'][:, 0], res_gpd['row_coord'][:, 1], color='blue')
+ax.scatter(res_gpd['col_coord'][:, 0], res_gpd['col_coord'][:, 1], color='red')
+for i in range(n_g):
+    ax.text(res_gpd['row_coord'][i, 0], res_gpd['row_coord'][i, 1], f'Gp {i+1}')
+for j in range(n_c):
+    ax.text(res_gpd['col_coord'][j, 0], res_gpd['col_coord'][j, 1], f'Col {j+1}')
+plt.show()
+
